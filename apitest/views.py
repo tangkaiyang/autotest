e4bc9d32bf2531e52f56d855b6a9e974c59f6b85
tangkaiyang <945541696@qq.com>
2019-07-10 08:10:28 +0800

    report.html有问题

--- apitest/views.py
+++ apitest/views.py
@@ -1,3 +1,4 @@
+import pymysql
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
@@ -62,3 +63,23 @@ def apis_manage(request):
     username = request.session.get('user', '')
     apis_list = Apis.objects.all()
     return render(request, "apis_manage.html", {"user": username, "apiss": apis_list})
+
+
+# 测试报告
+@login_required
+def test_report(request):
+    username = request.session.get('user', '')
+    apis_list = Apis.objects.all()
+    apis_count = Apis.objects.all().count()  # 统计接口数
+    db = pymysql.connect(user='root', db='autotest', passwd='root', host='127.0.0.1', port=3306)
+    cursor = db.cursor()
+    sql1 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apiststatus=1'
+    aa = cursor.execute(sql1)
+    apis_pass_count = [row[0] for row in cursor.fetchmany(aa)][0]
+    sql2 = 'SELECT count(id) FROM apitest_apis WHERE apitest_apis.apistatus=0'
+    bb = cursor.execute(sql2)
+    apis_fail_count = [row[0] for row in cursor.fetchmany(bb)][0]
+    db.close()
+    return render(request, "report.html",
+                  {"user": username, "apiss": apis_list, "apiscounts": apis_count, "apis_pass_counts": apis_pass_count,
+                   "apis_fail_counts": apis_fail_count})  # 把值赋给apiscounts变量
